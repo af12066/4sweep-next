06e8552ae3288f6fe7ce4534ec14c44642af9742
Takuma Hashimoto <takumaxd+github@gmail.com>
2018-07-08 15:50:15 +0900

    Add leaflet mock

---
+++ components/map.vue
@@ -0,0 +1,42 @@
+<template>
+  <l-map
+    :zoom="zoom"
+    :center="center"
+  >
+    <l-tile-layer
+      :url="url"
+      :attribution="attribution" />
+    <l-marker
+      :lat-lng="marker" />
+  </l-map>
+</template>
+
+<script>
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      zoom: 13,
+      center: L.latLng(47.413220, -1.219482),
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: L.latLng(47.413220, -1.219482),
+    };
+  },
+};
+</script>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,17 +1,34 @@
 <template>
-  <div>
-    <h1>Login Completed!</h1>
-    <p>{{ $auth.$storage.getLocalStorage('_token.social') }}</p>
-  </div>
+  <v-layout
+    row
+    wrap
+  >
+    <v-flex
+      xs4
+    >
+      <h1>Login Completed!</h1>
+      <p>{{ $auth.$storage.getLocalStorage('_token.social') }}</p>
+    </v-flex>
+    <v-flex
+      xs8
+    >
+      <Map />
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 import * as type from '../store/action-types';
+import Map from '~/components/map';
 
 export default {
+  components: {
+    Map,
+  },
   created() {
     this.$store.dispatch(type.FETCH_SELF_USER_DATA);
   },
+
 };
 </script>
 
