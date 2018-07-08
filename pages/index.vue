6b51b82b623237283309875544072042c0f0dce6
Takuma Hashimoto <takumaxd+github@gmail.com>
2018-07-08 15:16:56 +0900

    update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,51 @@
 <template>
-  <h1>Hello world!</h1>
+  <v-jumbotron>
+    <v-layout
+      row
+      text-xs-center
+    >
+      <v-flex
+        xs12
+      >
+        <h1
+          class="display-3 font-weight-regular"
+        >4sweep-next</h1>
+        <p class="subheading">
+          Next generation 4sweep, which help all superusers to edit venues.
+        </p>
+      </v-flex>
+    </v-layout>
+    <v-layout
+      row
+      text-xs-center
+    >
+      <v-flex
+        xs6
+        offset-xs3
+      >
+        <v-btn
+          dark
+          class="pink"
+          large
+          @click="authenticate"
+        >Sign in with Foursquare</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-jumbotron>
 </template>
+
+<script>
+  export default {
+    methods: {
+      authenticate() {
+        this.$auth.loginWith('social');
+      },
+    },
+  };
+</script>
+
+<style scoped>
+.subheading {
+  padding: 1rem 0;
+}
+</style>
