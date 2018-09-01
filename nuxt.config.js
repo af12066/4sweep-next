0ff315b0bc5f839e6fc239005de40fe4bcb80d55
Takuma Hashimoto <takumaxd+github@gmail.com>
2018-09-02 00:15:11 +0900

    Add OGP

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,11 +48,17 @@ module.exports = {
   head: {
     htmlAttrs: {
       lang: 'en',
+      prefix: 'og: http://ogp.me/ns# website: http://ogp.me/ns/website#',
     },
     meta: [
       {'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge'},
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {name: 'description', content: 'Next generation 4sweep'},
+      {property: 'og:title', content: '4sweep-next'},
+      {property: 'og:description', content: 'Next generation 4sweep'},
+      {property: 'og:type', content: 'website'},
+      {property: 'og:url', content: process.env.BASE_URL || 'http://localhost:3000'},
     ],
   },
 };
